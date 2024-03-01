cf34f3a9c2bdf056e1e05d2c15c5f434a4b50e4c
Drin <aldrinisit3196@gmail.com>
2024-03-01 22:39:32 +0800

    Add: todos icon and display todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -12,13 +13,28 @@ function TodoList() {
       const newTodos = [todo, ...todos];
 
       setTodos(newTodos);
-      console.log(...todos);
     };
+
+    const completeTodo = id =>{
+      let updatedTodos = todos.map(todo => {
+        if(todo.id === id){
+          todo.isComplete = !todo.isComplete
+        }
+        return todo;
+      });
+      setTodos(updatedTodos)
+    }
+
+    
 
   return (
     <div>
         <h1>What the fuck Today?</h1>
         <TodoForm onSubmit={addTodo}/>
+        <Todo 
+        todos={todos}
+        completeTodo={completeTodo}
+        />
     </div>
   );
 }
